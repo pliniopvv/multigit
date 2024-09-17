ab13f7e2cc666aa0919e48a2501d1e54947e0f5d
Plínio Pvv <gangss2@hotmail.com>
2024-09-17 13:12:45 -0300

    finish

--- src/Kernel/__init__.py
+++ src/Kernel/__init__.py
@@ -3,5 +3,6 @@ from Configuration import Configuration
 class Kernel:
     def __init__(self):
         self.config = Configuration()
+        self.tray = None
 
 ImplKernel = Kernel()
--- src/Window/__init__.py
+++ src/Window/__init__.py
@@ -1,10 +1,21 @@
+from Kernel import ImplKernel as Kernel
 from Window.Screens import ListRepository
+
+from git import Repo
 from pystray import Icon, MenuItem as Item
+
 from PIL import Image
+import schedule
+import time
 
 import threading
 import psutil
 import os
+
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 def upPriority(win):
     p = psutil.Process(os.getpid())
@@ -13,6 +24,7 @@ def upPriority(win):
 
 def createThread(win):
     threading.Thread(daemon=True, target=lambda : upPriority(win)).start()
+
 
 __ITEMS = []
 
@@ -29,7 +41,7 @@ def makeSystemTray():
         'test name',
         icon=IconImage(),
         menu=tuple(__ITEMS))
-    
+    Kernel.tray = icon
     return icon
 
 def makeItem(title, action):
@@ -38,6 +50,36 @@ def makeItem(title, action):
             action=action
     ))
 
+def checkRepos():
+    logger.info("Executando checagem ...")
+    repos = Kernel.config.getRepositorios()
+    hasUpdate = []
+    for model in repos:
+        try:
+            repo = Repo(model.path)
+            repo.remotes.origin.fetch()
+            remote_commits = repo.git.rev_list('HEAD..origin/master','--count')
+            if int(remote_commits) > 0:
+                hasUpdate.append(model.name)
+                logger.info("Updates detectados no " + model.name + " disparando notificação .")
+                # Kernel.tray.notify(model.name + " com updates !")
+        except Exception:
+            logger.error(model.name + " sem origin/master para checar.")
+    if (len(hasUpdate) > 0):
+        Kernel.tray.notify("Os repositórios " + ",".join(hasUpdate) + " possuem updates !")
+    logger.info("Checagem finalizada .")
+
+
+def configSchedule():
+    # schedule.every(1).hours.do(checkRepos)
+    schedule.every(10).seconds.do(checkRepos)
+    logger.info("Configurado Schedule .")
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
 def startSystemTray():
     makeItem('Repositórios', lambda : createThread(ListRepository))
     makeItem('Sair', close)
@@ -45,4 +87,6 @@ def startSystemTray():
     process = psutil.Process(os.getpid())
     process.nice(psutil.ABOVE_NORMAL_PRIORITY_CLASS)
 
+    threading.Thread(target=configSchedule, daemon=True).start()
+    # configSchedule()
     makeSystemTray().run()
